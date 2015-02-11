09c0ca8b48de1382375dbc7bd2388df6cf077982
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-02-11 09:52:19 +0100

    Add initial config for carrier wave

---
+++ spec/dummy/config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage = :file
+    config.enable_processing = false
+  else
+    config.storage = :file
+  end
+end
