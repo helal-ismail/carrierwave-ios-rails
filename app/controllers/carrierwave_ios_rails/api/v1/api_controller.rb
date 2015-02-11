e6678bab9aaf1e782df94ee81ff1c3218101034b
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-02-11 14:19:21 +0100

    Fix indentation
    [c36e69993f6bf887a0433d20849d44719bb6e615]

--- app/controllers/carrierwave_ios_rails/api/v1/api_controller.rb
+++ app/controllers/carrierwave_ios_rails/api/v1/api_controller.rb
@@ -1,17 +1,17 @@
 module CarrierwaveIosRails
-	class API::V1::ApiController < ActionController::Base
-		respond_to :json
+  class API::V1::ApiController < ActionController::Base
+    respond_to :json
 
-		rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-		decent_configuration do
-			strategy DecentExposure::StrongParametersStrategy
-		end
+    decent_configuration do
+      strategy DecentExposure::StrongParametersStrategy
+    end
 
-		private
+    private
 
-		def record_not_found
-			render json: { errors: I18n.t('errors.record_not_found') }, status: :not_found
-		end
-	end
+    def record_not_found
+      render json: { errors: I18n.t('errors.record_not_found') }, status: :not_found
+    end
+  end
 end
